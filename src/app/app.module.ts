b4f9bf7a02dedcb7799b589d85d652749d7ffd79
Gabriel Gualberto <gabrielgualbertooliveira@gmail.com>
2022-11-12 02:11:40 -0300

    add new components & bootstrap desing replicated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { AwardsComponent } from './awards/awards.component';
+import { EducationComponent } from './education/education.component';
+import { ExpComponent } from './exp/exp.component';
+import { InterestsComponent } from './interests/interests.component';
+import { SkillsComponent } from './skills/skills.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    AwardsComponent,
+    EducationComponent,
+    ExpComponent,
+    InterestsComponent,
+    SkillsComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
